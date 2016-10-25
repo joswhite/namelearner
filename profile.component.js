ebe8d3a0d0592ac378419d57dae49cd466116af1
Joseph White <joseph.white@microfocus.com>
2016-10-25 16:32:04 -0600

    Fixes from Joe Hawkins. An example of the controllerAs syntax.

--- profile.component.js
+++ profile.component.js
@@ -1,12 +1,13 @@
-angular
-    .module('app')
+angular.module('app')
+    .controller('ProfileComponentController', [
+        'profileService', 
+        function(profileService) {
+            this.profile = profileService;
+        }
+    ])
+
     .component('profile', {
         templateUrl: 'profile.component.html',
-        controller: function() {
-            this.picture = 'default.jpg';
-            this.name = 'Ana Swanlund';
-            this.surname = 'Swanlund';
-            this.phoneNumbers = ['425.881.4024','206.114.8613'];
-            this.address = ['429 East Bay Drive', 'Suite 30, Apt 25', 'Fort Collins, Colorado'];
-        }
+        controller: 'ProfileComponentController',
+        controllerAs: '$ctrl'
     });
