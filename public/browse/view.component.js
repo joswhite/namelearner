e501e553d08b4d206c00d4f84797fc8529271daa
Joseph White <joseph.white@microfocus.com>
2016-11-03 18:46:01 -0600

    Use ngResource instead of $http for REST API calls.

--- public/browse/view.component.js
+++ public/browse/view.component.js
@@ -2,18 +2,7 @@ angular.module('app')
 	.component('viewComponent', {
 		templateUrl: 'browse/view.component.html',
 		controller: ['peopleService', function(peopleService) {
-			/*this.person = peopleService.get({ id: '581b936ea1c97717a88128cb' });*/
-			var self = this;
-			window.newObj = this;
-			peopleService.getPeople().then(
-				function(people) {
-					self.person = people.data[0];
-					console.log(self.person, people.data[0]);
-					console.log('of' + JSON.stringify(people));
-				}, function(error) {
-					throw error;
-				}
-			)
+			this.person = peopleService.get({ id: '581b936ea1c97717a88128cb' });
 
 			this.update = function(name, phoneNumber, addressLine, index) {
 				//alert(JSON.stringify([name, phoneNumber, addressLine, index]));
--- public/services/people.service.js
+++ public/services/people.service.js
@@ -1,40 +1,6 @@
 angular.module('app')
-	.factory("peopleService", ['$http', '$resource', PeopleService]);
+	.factory("peopleService", ['$resource', PeopleService]);
 
-function PeopleService($http, $resource) {
-	var service = {
-		addPerson: addPerson,
-		addPeople: addPeople,
-		deletePerson: deletePerson,
-		getPeople: getPeople,
-		getPerson: getPerson,
-		updatePerson: updatePerson
-	};
-	return service;
-
-	//return $resource('/api/people/:id');
-
-	function addPerson() {
-
-	};
-
-	function addPeople() {
-
-	};
-
-	function deletePerson(id) {
-
-	};
-
-	function getPeople() {
-		return $http.get('/api/people');
-	};
-
-	function getPerson(id) {
-		return $http.get('/api/people/4Db');
-	};
-
-	function updatePerson(id) {
-
-	};
+function PeopleService($resource) {
+	return $resource('/api/people/:id');
 }
