7ccbb22df48df2ed66257f317242f01dc8cbacbd
Joseph White <joseph.white@microfocus.com>
2017-04-19 08:41:39 -0600

    Update restart.sh for testing

--- restart.sh
+++ restart.sh
@@ -3,13 +3,13 @@
 # Add output to same log that Forever.js uses
 node ./node_modules/forever/bin/forever stopall
 
-echo "   ***git pull"
+echo "   ***git pull" &>> ~/.forever/forever.log
 git pull &>> ~/.forever/forever.log
 
-echo "   ***npm i"
+echo "   ***npm i" &>> ~/.forever/forever.log
 npm i &>> ~/.forever/forever.log
 
-echo "   ***gulp build"
+echo "   ***gulp build" &>> ~/.forever/forever.log
 node ./node_modules/gulp/bin/gulp.js build &>> ~/.forever/forever.log
 
 node ./node_modules/forever/bin/forever start forever.json
