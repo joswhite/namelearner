974bdf3480a439564097a54ff21d3d821ac4a774
Joseph White <joseph.white@microfocus.com>
2017-04-19 09:05:25 -0600

    Make restart.sh have no downtime

--- restart.sh
+++ restart.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Add output to same log that Forever.js uses
-node ./node_modules/forever/bin/forever stopall
-
 echo -e "\nforever/updating# git pull" &>> ~/.forever/forever.log
 git pull &>> ~/.forever/forever.log
 
@@ -12,6 +10,8 @@ npm i &>> ~/.forever/forever.log
 echo -e "\nforever/updating# gulp build" &>> ~/.forever/forever.log
 node ./node_modules/gulp/bin/gulp.js build &>> ~/.forever/forever.log
 
-node ./node_modules/forever/bin/forever start forever.json
+node ./node_modules/forever/bin/forever restartall
 
+#node ./node_modules/forever/bin/forever start forever.json
+#node ./node_modules/forever/bin/forever stopall
 
