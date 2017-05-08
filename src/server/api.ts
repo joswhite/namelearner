d3ba4703ef97a0cbb656e619aee915cd7c0d66b5
Joseph White <joseph.white@microfocus.com>
2017-05-08 10:35:05 -0600

    Flesh out how permissions will be handled

--- src/server/api.ts
+++ src/server/api.ts
@@ -2,16 +2,17 @@
 import express = require('express');
 const router = express.Router();
 
+import * as permissions from './controllers/permissions';
 import people = require('./controllers/people');
 import users = require('./controllers/users');
 
 // Use traditional naming conventions
-router.post('/people/:id/upload', people.upload);
-router.get('/people/:id', people.show);
+router.post('/people/:id/upload', permissions.personId, people.upload);
+router.get('/people/:id', permissions.personId, people.show);
 router.get('/people', people.list);
 router.post('/people', people.create);
-router.put('/people/:id', people.update);
-router.delete('/people/:id', people.remove);
+router.put('/people/:id', permissions.personId, people.update);
+router.delete('/people/:id', permissions.personId, people.remove);
 
 router.get('/users/:id', users.show);
 router.get('/users', users.list);
